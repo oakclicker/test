efcb33675f0d9ca7b8edb1d2a0eb69dcbe995b05
OAK Crypton <165552451+oakcrypton@users.noreply.github.com>
2024-04-28 19:53:56 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
+
 function App() {
+  const [userData, setUserData] = useState({});
+
+  useEffect(() => {
+    fetch('/user')
+      .then(response => response.json())
+      .then(data => setUserData(data))
+      .catch(error => console.error('Ошибка:', error));
+  }, []);
+
   return (
     <div className="App">
-    Work
+      <h1>Информация о пользователе</h1>
+      <p>Username: {userData.username}</p>
+      <p>Fullname: {userData.fullname}</p>
+      <p>User ID: {userData.user_id}</p>
     </div>
   );
 }
