e8c4c0ed77613659740721384802c2291181cc1b
OAK Crypton <165552451+oakcrypton@users.noreply.github.com>
2024-04-29 20:03:59 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,52 +1,27 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
   const [userData, setUserData] = useState(null);
-  const incrementButtonRef = useRef(null);
 
   useEffect(() => {
     const telegramApp = window.Telegram.WebApp;
-
-    // Функция для обновления данных пользователя
-    const updateUserData = () => {
-      const userData = telegramApp.initDataUnsafe.user;
-      setUserData(userData);
-    };
-
-    // Обновляем данные пользователя при загрузке страницы
-    updateUserData();
-
-    // Добавляем обработчик события для кнопки увеличения баланса
-    const incrementButton = incrementButtonRef.current;
-    if (incrementButton) {
-      incrementButton.addEventListener('click', () => {
-        const balanceElement = document.getElementById("balance");
-        const currentBalance = parseInt(balanceElement.innerText.split(": ")[1]);
-        balanceElement.innerText = `Balance: ${currentBalance + 1}`;
-      });
-    }
-
-    // Очистка слушателя событий при размонтировании компонента
-    return () => {
-      if (incrementButton) {
-        incrementButton.removeEventListener('click');
-      }
-    };
+    const userData = telegramApp.initDataUnsafe.user;
+    setUserData(userData);
   }, []);
 
   return (
     <div className="App">
       {userData && (
         <div id="usercard">
-          <p>{`${userData.first_name} ${userData.last_name}`}</p>
-          <p>{`User ID: ${userData.id}`}</p>
-          <p>{`Username: ${userData.username}`}</p>
-          <img src={userData.photo_url} alt="User Avatar" id="avatar" />
+          <p>User ID: {userData.id}</p>
+          <p>Is Bot: {userData.isBot ? 'Yes' : 'No'}</p>
+          <p>First Name: {userData.first_name}</p>
+          <p>Last Name: {userData.last_name}</p>
+          <p>Username: {userData.username}</p>
+          <p>Language Code: {userData.language_code}</p>
         </div>
       )}
-      <button ref={incrementButtonRef} id="incrementButton">Increment Balance</button>
-      <p id="balance">Balance: 0</p>
     </div>
   );
 }
