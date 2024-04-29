5bdd96889ae9e0a88a62d6cc974301ce0665f66a
OAK Crypton <165552451+oakcrypton@users.noreply.github.com>
2024-04-29 19:55:32 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,21 +2,44 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [userData, setUserData] = useState({});
+  const [userData, setUserData] = useState(null);
 
   useEffect(() => {
-    fetch('http://62.217.182.104:3000/user')
-      .then(response => response.json())
-      .then(data => setUserData(data))
-      .catch(error => console.error('Ошибка:', error));
-  }, []);
+    const telegramApp = window.Telegram.WebApp;
+
+    // Функция для обновления данных пользователя
+    const updateUserData = () => {
+      const userData = telegramApp.initDataUnsafe.user;
+      setUserData(userData);
+    };
+
+    // Обновляем данные пользователя при загрузке страницы
+    updateUserData();
+
+    // Добавляем обработчик события для кнопки увеличения баланса
+    const incrementButton = document.getElementById("incrementButton");
+    incrementButton.addEventListener('click', () => {
+      const balanceElement = document.getElementById("balance");
+      const currentBalance = parseInt(balanceElement.innerText.split(": ")[1]);
+      balanceElement.innerText = `Balance: ${currentBalance + 1}`;
+    });
+
+    // Очистка слушателя событий при размонтировании компонента
+    return () => {
+      incrementButton.removeEventListener('click');
+    };
+  }, []); // Пустой массив зависимостей для запуска эффекта только один раз при монтировании компонента
 
   return (
     <div className="App">
-      <h1>Информация о пользователе</h1>
-      <p>Username: {userData.username}</p>
-      <p>Fullname: {userData.fullname}</p>
-      <p>User ID: {userData.user_id}</p>
+      {userData && (
+        <div id="usercard">
+          <p>{`${userData.first_name} ${userData.last_name}`}</p>
+          <p>{`User ID: ${userData.id}`}</p>
+          <p>{`Username: ${userData.username}`}</p>
+          <img src={userData.photo_url} alt="User Avatar" id="avatar" />
+        </div>
+      )}
     </div>
   );
 }
