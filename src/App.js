30fed7f68b652cb1de1ef78aedec2755b69a83ea
oakcrypton <oak@oakton.ru>
2024-04-30 12:42:21 +0500

    efs

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 function App() {
   const [userData, setUserData] = useState(null);
+  const [balance, setBalance] = useState(0);
 
   useEffect(() => {
     const telegramApp = window.Telegram.WebApp;
@@ -10,12 +11,23 @@ function App() {
     setUserData(userData);
   }, []);
 
+  const handleAddBalance = () => {
+    setBalance(prevBalance => prevBalance + 1);
+  };
+
   return (
     <div className="App">
       {userData && (
         <div id="usercard">
-          <p>User ID: {userData.id}</p>
-          <p>Fullname: {userData.first_name} {userData.last_name}</p>
+          <div className="user-panel">
+            <img src={userData.avatar} alt="Avatar" className="avatar" />
+            <p>User ID: {userData.id}</p>
+            <p>Fullname: {userData.first_name} {userData.last_name}</p>
+          </div>
+          <p className="balance">Balance: {balance}</p>
+          <button className="add-balance-button" onClick={handleAddBalance}>
+            Add Balance
+          </button>
         </div>
       )}
     </div>
