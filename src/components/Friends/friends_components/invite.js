e18d73c1b36ddf0e192563ca2927943e10a3fd40
OAK Crypton <165552451+oakcrypton@users.noreply.github.com>
2024-05-01 13:09:12 +0500

    Update invite.js

--- src/components/Friends/friends_components/invite.js
+++ src/components/Friends/friends_components/invite.js
@@ -6,12 +6,12 @@ import coin_icon from '../friends_components/friends_icons/coin.png';
 const Invite = () => {
   return (
    <button className='referal_invite'>
-      <img src={present_icon} className='present_icon transparent' />
+      <img src={present_icon} alt='Present Icon' className='present_icon transparent' />
       <div className='invite_text transparent'>
          <p className='invite_title transparent'>Invite a friend</p>
          <div className='cash transparent'>
             <span className='check transparent'>8600</span>
-            <img className='coin transparent' src={coin_icon} />
+            <img alt='Coin Icon' className='coin transparent' src={coin_icon} />
          </div>
       </div>
       <p></p>
