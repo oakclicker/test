cdcb4e3e1c312f6a3d490c190d7837c44cf4eb8d
OAK Crypton <165552451+oakcrypton@users.noreply.github.com>
2024-05-01 12:40:23 +0500

    Update friends_list.js

--- src/components/Friends/friends_components/friends_list.js
+++ src/components/Friends/friends_components/friends_list.js
@@ -3,23 +3,23 @@ import '../Friends.css';
 import user from './friends_icons/User_icon.svg';
 import coin_icon from '../friends_components/friends_icons/coin.png';
 
-const Friends_list = () => {
+const FriendsList = () => {
   return (
     <div className='friend_card'>
-        <img src={user} className='user_icon' />
+        <img src={user} className='user_icon' alt="User icon" />
         <div className='friend_text transparent'>
           <p className='friend_name transparent'>Friend Name</p>
           <div className='friend_info transparent'>
             <span className='check transparent'>8600</span>
-            <img className='coin transparent' src={coin_icon} />
+            <img className='coin transparent' src={coin_icon} alt="Coin icon" />
           </div>
         </div>
         <div className='bonus_display transparent'>
           <span className='friend_bonus transparent'>+2k</span>
-          <img className='coin transparent transparent' src={coin_icon} />
+          <img className='coin transparent transparent' src={coin_icon} alt="Coin icon" />
         </div>
     </div>
   );
 }
 
-export default Friends_list;
+export default FriendsList;
