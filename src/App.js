5a5e905442fc6327107353140f84f1c377af6cb7
Artem <157585581+ArtemSilin1@users.noreply.github.com>
2024-05-03 10:58:35 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -67,7 +67,7 @@ function App() {
         {userData && (
           <div id="usercard">
             <div className="user-panel">
-               <img src="https://t.me/i/userpic/320/{userData.username}.jpg" alt="Avatar" className="avatar transparent" />
+               <img src={`https://t.me/i/userpic/320/${userData.username}.jpg`} alt="Avatar" className="avatar transparent" />
               <div className='userInfo_container transparent'>
                 <p className='transparent user_name'>{userData.first_name} {userData.last_name}</p>
                 <p className='transparent user_id'>ID: {userData.id}</p>
