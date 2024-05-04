25cf661133200a913022ca0066dec536752dbc04
OAK Crypton <165552451+oakcrypton@users.noreply.github.com>
2024-05-04 09:15:32 +0500

    Update EarnTask.js

--- src/components/Earn/earn_components/EarnTask.js
+++ src/components/Earn/earn_components/EarnTask.js
@@ -4,26 +4,24 @@ import Dialoge from './earn_icons/Dialoge.png';
 import SmallCoin from './earn_icons/small_coin.png';
 import ArrowRight from './earn_icons/arrow_to_right.svg';
 
-
 const EarnTask = () => {
-   return (
-      <div>
-        <a className='daily_earn' >
-          <img src={Dialoge} alt='Calendar' className='calendar_img' />
-          <div className='task_text'>
-            <p className='task_name'>Daily reward</p>
-            <p className='task_price'>
-              <img src={SmallCoin} alt='small coin' className='small_coin' />
-              <p>+5 450 000</p>
-            </p>
-          </div>
-          <p></p>
-          <a>
-              <img src={ArrowRight} className='arrow_to_right' alt="Arrow To Right" />
-          </a> 
-        </a>
-     </div>
-   );
- }
- 
- export default EarnTask;
+  return (
+    <div>
+      {/* Исправлено: добавлен атрибут href */}
+      <a href='#' className='daily_earn'>
+        <img src={Dialoge} alt='Calendar' className='calendar_img' />
+        <div className='task_text'>
+          <p className='task_name'>Daily reward</p>
+          <p className='task_price'>
+            <img src={SmallCoin} alt='small coin' className='small_coin' />
+            <p>+5 450 000</p>
+          </p>
+        </div>
+        {/* Исправлено: добавлен закрывающий тег */}
+        <p><a href='#'><img src={ArrowRight} className='arrow_to_right' alt="Arrow To Right" /></a></p>
+      </a>
+    </div>
+  );
+}
+
+export default EarnTask;
