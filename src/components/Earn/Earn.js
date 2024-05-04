9e9c5087403d4f4466e11b6ee5aeab4aec838c0b
oakcrypton <oak@oakton.ru>
2024-05-04 10:11:27 +0500

    fdfdf

--- src/components/Earn/Earn.js
+++ src/components/Earn/Earn.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './Earn.css';
-import IntroIcon from './earn_components/earn_icons/Earn_main_icon.png';
-import Calendar from './earn_components/earn_icons/calendar.png';
-import SmallCoin from './earn_components/earn_icons/small_coin.png';
-import ArrowRight from './earn_components/earn_icons/arrow_to_right.svg';
-import EarnTask from './earn_components/EarnTask.js';
+import IntroIcon from './earn_icons/Earn_main_icon.png';
+import Calendar from './earn_icons/calendar.png';
+import SmallCoin from './earn_icons/small_coin.png';
+import ArrowRight from './earn_icons/arrow_to_right.svg';
+import Telegram from './earn_icons/tgico.png';
+import Twitter from './earn_icons/xcom.png';
+import VK from './earn_icons/vk.png';
+
 
 const Earn = () => {
   return (
-    <div>
+    <div className='daily'>
       <div className='intro_header'>
         <img src={IntroIcon} alt='Intro Coin' className='intro_icon' />
         <h2 className='intro_title'>Earn more OAK</h2>
@@ -17,8 +20,7 @@ const Earn = () => {
       <div className='earn__container'>
         <h3>Daily tasks</h3>
 
-        {/* Исправлено: добавлен атрибут href */}
-        <a href='#' className='daily_earn'>
+        <h className='daily_earn'>
           <img src={Calendar} alt='Calendar' className='calendar_img' />
           <div className='task_text'>
             <p className='task_name'>Daily reward</p>
@@ -27,18 +29,69 @@ const Earn = () => {
               <p>+5 450 000</p>
             </p>
           </div>
-          {/* Исправлено: добавлен закрывающий тег */}
-          <p><a href='#'><img src={ArrowRight} className='arrow_to_right' alt="Arrow To Right" /></a></p>
-        </a>
+          <p><h><img src={ArrowRight} className='arrow_to_right' alt="Arrow To Right" /></h></p>
+        </h>
       </div>
       
       <div className='earn__container'>
         <h3>Tasks list</h3>
 
-        <EarnTask />
-        <EarnTask />
-        <EarnTask />
-        <EarnTask />
+        <div className='task1'>
+          <h className='task_list'>
+            <img src={Telegram} alt='Calendar' className='task_img' />
+              <div className='task_text'>
+                <p className='task_name'>Subscribe Channel</p>
+                <p className='task_price'>
+                <img src={SmallCoin} alt='small coin' className='small_coin' />
+                <p>+5 450 000</p>
+                </p>
+              </div>
+                <p><h><img src={ArrowRight} className='arrow_to_right' alt="Arrow To Right" /></h></p>
+                </h>
+          </div>
+
+        <div className='task2'>
+          <h className='task_list'>
+            <img src={Telegram} alt='Calendar' className='task_img' />
+              <div className='task_text'>
+                <p className='task_name'>Subscribe Group</p>
+                <p className='task_price'>
+                <img src={SmallCoin} alt='small coin' className='small_coin' />
+                <p>+5 450 000</p>
+                </p>
+              </div>
+                <p><h><img src={ArrowRight} className='arrow_to_right' alt="Arrow To Right" /></h></p>
+                </h>
+          </div>
+          
+          <div className='task3'>
+          <h className='task_list'>
+            <img src={Twitter} alt='Calendar' className='task_img' />
+              <div className='task_text'>
+                <p className='task_name'>Subscribe Twitter</p>
+                <p className='task_price'>
+                <img src={SmallCoin} alt='small coin' className='small_coin' />
+                <p>+5 450 000</p>
+                </p>
+              </div>
+                <p><h><img src={ArrowRight} className='arrow_to_right' alt="Arrow To Right" /></h></p>
+                </h>
+          </div>
+          
+          <div className='task4'>
+          <h className='task_list'>
+            <img src={VK} alt='Calendar' className='task_img' />
+              <div className='task_text'>
+                <p className='task_name'>Subscribe Vk</p>
+                <p className='task_price'>
+                <img src={SmallCoin} alt='small coin' className='small_coin' />
+                <p>+5 450 000</p>
+                </p>
+              </div>
+                <p><h><img src={ArrowRight} className='arrow_to_right' alt="Arrow To Right" /></h></p>
+                </h>
+          </div>
+          
       </div>
       
     </div>
