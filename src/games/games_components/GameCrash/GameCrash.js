4fa437bd7039d2129269b84fdf93d51e60eb5558
ArtemSilin1 <glitchplague6qq@gmail.com>
2024-05-18 17:20:09 +0500

    AAA

--- src/games/games_components/GameCrash/GameCrash.js
+++ src/games/games_components/GameCrash/GameCrash.js
@@ -14,17 +14,44 @@ const GameCrash = ({ isOpen, onClose }) => {
       >
          <div className='Game'>
 
-            <div className='game_header'>
-               <img alt='back' src={BackIcon} className='back_button' onClick={onClose} />
-               <h2 className='game__title'>Game Crash</h2>
-               <img alt='game icon' src={SlotsIcon} className='GameIcon' />
+            <div className='header__container'>
+               <div className='game_header'>
+                  <img alt='back' src={BackIcon} className='back_button' onClick={onClose} />
+                  <h2 className='game__title'>Game Crash</h2>
+                  <img alt='game icon' src={SlotsIcon} className='GameIcon' />
+               </div>
+               <p className='header_balance_display'>balance: 10000000</p>
             </div>
             
-            
             <div className='crash__content'>
-               <p>Crash</p>
+               <div className='crash_history'>
+                  <p>Game history</p>
+                  <div className='crash_history_elements'>
+                     <p className='c_history_element red'>x25.3</p>
+                     <p className='c_history_element orange'>x8</p>
+                     <p className='c_history_element green'>x1.7</p>
+                     <p className='c_history_element orange'>x11.04</p>
+                     <p className='c_history_element red'>x18.2</p>
+                     <p className='c_history_element green'>x2.1</p>
+                     <p className='c_history_element orange'>x7.2</p>
+                     <p className='c_history_element green'>x3.7</p>
+                     <p className='c_history_element red'>x46.2</p>
+                  </div>
+               </div>
                <div className='crash__gameplay'>
-                  <p>это контейнер</p>
+                  <p>тут геймплей</p>
+               </div>
+               <div className='crash_bets'>
+                  <div className='crash_bets_container'>
+                     <div>
+                        <p>Bet</p>
+                        <input type='number' placeholder='Your bet' className='Game_input Crash_input'></input>
+                     </div>
+                     <div className='auto_get_container'>
+                        <p>Auto</p>
+                        <input type='number' placeholder='Your bet' className='Game_input Crash_input_auto'></input>
+                     </div>
+                  </div>
                </div>
                <button className='crash_button'>Roll</button>
             </div>
